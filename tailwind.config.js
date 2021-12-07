199ebe17112a6f02046323ec0a9ebbbd253bbc9f
Anton Sjölander <anton.sjolander@codemill.se>
2021-12-07 13:04:11 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     customForms: (theme) => ({
       default: {
